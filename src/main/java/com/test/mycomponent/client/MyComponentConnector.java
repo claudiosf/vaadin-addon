fbcb7e23c56e7eb2e0e1b22d4f52994475a1eb54
claudio <claudio.ferrete@gmail.com>
2012-11-02 00:32:36 +0000

    Changed connector to data binding

--- src/main/java/com/test/mycomponent/client/MyComponentConnector.java
+++ src/main/java/com/test/mycomponent/client/MyComponentConnector.java
@@ -1,10 +1,9 @@
 package com.test.mycomponent.client;
 
-import com.vaadin.client.communication.StateChangeEvent;
-import com.vaadin.client.ui.AbstractComponentConnector;
-import com.vaadin.shared.ui.Connect;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Widget;
+import com.vaadin.client.ui.AbstractComponentConnector;
+import com.vaadin.shared.ui.Connect;
 
 @Connect(com.test.mycomponent.MyComponent.class)
 public class MyComponentConnector extends AbstractComponentConnector {
@@ -21,13 +20,5 @@ public class MyComponentConnector extends AbstractComponentConnector {
 	@Override
 	public MyComponentState getState() {
 		return (MyComponentState) super.getState();
-	}
-
-	@Override
-	public void onStateChanged(StateChangeEvent stateChangeEvent) {
-		super.onStateChanged(stateChangeEvent);
-
-		final String text = getState().text;
-		getWidget().setText(text);
 	}
 }
--- src/main/java/com/test/mycomponent/client/MyComponentState.java
+++ src/main/java/com/test/mycomponent/client/MyComponentState.java
@@ -1,9 +1,11 @@
 package com.test.mycomponent.client;
 
 import com.vaadin.shared.ComponentState;
+import com.vaadin.shared.annotations.DelegateToWidget;
 
 public class MyComponentState extends ComponentState {
 
+	@DelegateToWidget("setText")
     public String text;
 
 }
